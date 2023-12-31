c0f4471a31d7d05eee436bb520f2b2a62fad353d
schlaepfAIR <thomas@schlaepfer.li>
2023-12-31 01:07:04 +0100

    next try, shiftplan screen is not working anymore

--- lib/screens/rest_calculation_screen.dart
+++ lib/screens/rest_calculation_screen.dart
@@ -1,44 +1,86 @@
-// Import statements for using Flutter material components and formatting dates.
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart'; // Import for date formatting.
 import '../dialogs/shift_plan_dialog.dart';
 
-// RestCalculationScreen class - a StatelessWidget for calculating rest times.
 class RestCalculationScreen extends StatelessWidget {
   final dynamic flightData; // Flight data received from previous screen.
   final Function(Map<String, dynamic>) onShiftPlanCreated; // Function to handle shift plan creation.
 
-  // Constructor for RestCalculationScreen with required parameters.
   RestCalculationScreen({
     required this.flightData,
     required this.onShiftPlanCreated,
   });
 
+  Widget _statusLabel(String? status) {
+    Color color;
+    switch (status) {
+      case 'landed':
+        color = Colors.yellow;
+        break;
+      case 'scheduled':
+        color = Colors.blue;
+        break;
+      case 'en-route':
+        color = Colors.green;
+        break;
+      default:
+        color = Colors.grey; // Default color for other statuses or null
+    }
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+      color: color,
+      child: Text(
+        status?.toUpperCase() ?? 'N/A',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+
+  DateTime? _parseUtcTime(String? utcString) {
+    if (utcString == null) return null;
+    return DateTime.tryParse(utcString);
+  }
+
+  String _formatDuration(Duration duration) {
+    return '${duration.inHours}h ${duration.inMinutes.remainder(60)}m';
+  }
+
+  String _formatTime(DateTime? dateTime) {
+    if (dateTime == null) return 'N/A';
+    return DateFormat('HH:mm').format(dateTime);
+  }
+
+  TableRow _buildTableRow(String label, Widget value) {
+    return TableRow(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+          child: Text(label, style: TextStyle(fontWeight: FontWeight.bold)),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          child: value,
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Private function to parse UTC time string to DateTime.
-    DateTime? _parseUtcTime(String? utcString) {
-      if (utcString == null) return null;
-      return DateTime.tryParse(utcString);
-    }
-
-    // Private function to format Duration into Hours and Minutes.
-    String _formatDuration(Duration duration) {
-      return '${duration.inHours}h ${duration.inMinutes.remainder(60)}m';
-    }
-
-    // Private function to format DateTime into HH:MM format.
-    String _formatTime(DateTime? dateTime) {
-      if (dateTime == null) return 'N/A';
-      return DateFormat('HH:mm').format(dateTime);
-    }
-
-    // Extracting response data from flightData.
     var responseData = flightData['response'] ?? {};
-    bool isEnRoute = responseData['status'] == 'en-route'; // Check if the flight is en-route.
+
+    DateTime? departureTime = _parseUtcTime(responseData['dep_actual_utc']) ??
+                              _parseUtcTime(responseData['dep_estimated_utc']) ??
+                              _parseUtcTime(responseData['dep_time_utc']);
+
+    DateTime? arrivalTime = _parseUtcTime(responseData['arr_actual_utc']) ??
+                            _parseUtcTime(responseData['arr_estimated_utc']) ??
+                            _parseUtcTime(responseData['arr_time_utc']);
+
+    bool isEnRoute = responseData['status'] == 'en-route';
     String? calculatedDuration;
     if (isEnRoute) {
-      // Calculating duration if the flight is en-route.
       DateTime? startTime = _parseUtcTime(responseData['utc']);
       DateTime? endTime = _parseUtcTime(responseData['arr_actual_utc']) ??
           _parseUtcTime(responseData['arr_estimated_utc']) ??
@@ -50,67 +92,51 @@ class RestCalculationScreen extends StatelessWidget {
       }
     }
 
-    // List of widgets displaying the flight data.
-    var dataWidgets = <Widget>[
-      ListTile(
-        title: Text("Actual Time UTC"),
-        subtitle: Text(_formatTime(_parseUtcTime(responseData['utc']))),
-      ),
-      ListTile(
-        title: Text("Status"),
-        subtitle: Text(responseData['status']?.toString() ?? 'N/A'),
-      ),
-      ListTile(
-        title: Text("Flight IATA"),
-        subtitle: Text(responseData['flight_iata']?.toString() ?? 'N/A'),
-      ),
-      ListTile(
-        title: Text("Origin"),
-        subtitle: Text(responseData['dep_iata']?.toString() ?? 'N/A'),
-      ),
-      ListTile(
-        title: Text("Destination"),
-        subtitle: Text(responseData['arr_iata']?.toString() ?? 'N/A'),
-      ),
-      ListTile(
-        title: Text("Airline"),
-        subtitle: Text(responseData['airline_name']?.toString() ?? 'N/A'),
-      ),
-      ListTile(
-        title: Text("Calculated Duration"),
-        subtitle: Text(calculatedDuration ?? 'N/A'),
-      ),
-    ];
-
-    // Function to build the shift plan button.
-    Widget buildShiftPlanButton() {
-      return isEnRoute
-          ? ElevatedButton(
-              onPressed: () async {
-                Map<String, dynamic>? shiftPlanData =
-                    await showShiftPlanDialog(context);
-                if (shiftPlanData != null) {
-                  onShiftPlanCreated(shiftPlanData); // Triggering shift plan creation callback.
-                }
-              },
-              child: Text('Make Shiftplan'),
-            )
-          : Tooltip(
-              message: "Flight has to be en-route",
-              child: ElevatedButton(
-                onPressed: null, // Disabled button when flight is not en-route.
-                child: Text('Make Shiftplan'),
-              ),
-            );
-    }
-
-    // Adding the shift plan button to the list of widgets.
-    dataWidgets.add(buildShiftPlanButton());
-
-    // Returning a Scaffold with a ListView containing all data widgets.
     return Scaffold(
       body: ListView(
-        children: dataWidgets,
+        children: <Widget>[
+          Container(
+            color: Colors.yellow,
+            padding: EdgeInsets.all(10),
+            child: Text(
+              "The actual version displays all times in UTC",
+              style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Table(
+            columnWidths: const {
+              0: IntrinsicColumnWidth(),
+              1: FlexColumnWidth(),
+            },
+            children: [
+              _buildTableRow("Actual Time UTC", Text(_formatTime(departureTime))),
+              _buildTableRow("Status", _statusLabel(responseData['status']?.toString())),
+              _buildTableRow("Flight IATA", Text(responseData['flight_iata']?.toString() ?? 'N/A')),
+              _buildTableRow("Origin", Text(responseData['dep_iata']?.toString() ?? 'N/A')),
+              _buildTableRow("Destination", Text(responseData['arr_iata']?.toString() ?? 'N/A')),
+              _buildTableRow("Airline", Text(responseData['airline_name']?.toString() ?? 'N/A')),
+              _buildTableRow("Departure Time", Text(_formatTime(departureTime))),
+              _buildTableRow("Arrival Time", Text(_formatTime(arrivalTime))),
+              _buildTableRow("Calculated Duration", Text(calculatedDuration ?? 'N/A')),
+            ],
+          ),
+          isEnRoute ? ElevatedButton(
+            onPressed: () async {
+              Map<String, dynamic>? shiftPlanData =
+                  await showShiftPlanDialog(context);
+              if (shiftPlanData != null) {
+                onShiftPlanCreated(shiftPlanData);
+              }
+            },
+            child: Text('Make Shiftplan'),
+          ) : Tooltip(
+            message: "Flight has to be en-route",
+            child: ElevatedButton(
+              onPressed: null,
+              child: Text('Make Shiftplan'),
+            ),
+          ),
+        ],
       ),
     );
   }
